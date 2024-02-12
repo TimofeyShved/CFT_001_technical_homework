178cdefe8ac91ca4138b3a033607797393652cc5
TimofeyShved <timofeya123@inbox.ru>
2024-02-12 23:20:33 +0600

    option start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,35 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        String option = "-o /some/path -s -f -p result_ ";
+
+        Pattern patternFillter1 = Pattern.compile("-.?[^\\-]*");
+        Matcher matcherFillter1 = patternFillter1.matcher(option);
+        while (matcherFillter1.find()){
+            String optionValue = matcherFillter1.group();
+            System.out.println(optionValue);
+            switch (optionValue.charAt(1)) {
+                case 'o':
+                    //ListString.add(line);
+                    break;
+                case 'p':
+                    //ListInt.add(line);
+                    break;
+                case 's':
+                    //ListInt.add(line);
+                    break;
+                case 'f':
+                    //ListInt.add(line);
+                    break;
+                default:
+                    System.out.println("NULL");
+                    break;
+            }
+        }
+
+
+
+
         List<String> loadFiles = new ArrayList<>();
         loadFiles.add("in1.txt");
         loadFiles.add("in2.txt");
@@ -33,9 +62,6 @@ public class Main {
         for (WorkToFile work: listWorks){
             work.SaveFile(fillterValue);
         }
-
-
-
 
     }
 
